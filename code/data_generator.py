3ede329a8128ff48ccde97a7c1fe49867a73ce41
mkk4726 <mkk4726@naver.com>
2024-01-12 16:57:00 +0900

    edit data_generator and preprocessing

--- code/data_generator.py
+++ code/data_generator.py
@@ -27,6 +27,8 @@ import matplotlib.pyplot as plt
 
 # RandomGenerator
 
+# RandomGenerator
+
 class DR_Generator(tf.keras.utils.Sequence):
     '''
     tf.keras.utils.Sequence를 상속받아 Sequence를 커스텀해 사용
@@ -34,6 +36,7 @@ class DR_Generator(tf.keras.utils.Sequence):
     def __init__(self, 
                dir_path,
                mask_path=None,
+               use_mask=False,
                batch_size=4,
                img_size=(512, 512),
                dataset=None,
@@ -41,7 +44,8 @@ class DR_Generator(tf.keras.utils.Sequence):
                is_train=True):
         '''
         dir_path (str): image path
-        mask_path ([str]): mask path
+        mask_path ([str]): mask path , 순서는 HardExudate, Hemohedge, Microane, SoftExudates
+        use_mask (bool) : mask 사용 여부
         batch_size (int): batch_size
         img_size (224, 224, 3): 다루는 이미지 사이즈
         dataset (str) : 가져오려는 dataset 이름 , APTOS or FGADR or EyePacks      
@@ -49,10 +53,12 @@ class DR_Generator(tf.keras.utils.Sequence):
         '''
         self.dir_path = dir_path
         self.mask_path = mask_path
+        self.use_mask = use_mask
         self.batch_size = batch_size
         self.img_size = img_size
         self.dataset = dataset
         self.start_end_index = start_end_index
+        self.is_train=is_train
         
 
         # load_dataset()을 통해서 directory path에서 라벨과 이미지를 확인
@@ -73,11 +79,26 @@ class DR_Generator(tf.keras.utils.Sequence):
         input_images_paths.sort()
 
         # mask path 지정
-        # unsupervsion 떄는 필요없음
-        if self.mask_path:
-          mask_images_paths = (os.path.join(self.mask_path, "semantic"))
-          mask_images_paths.sort()
-
+        # mask : HardExudate, Hemohedge, Microane, SoftExudates
+        if self.use_mask:
+          ex_file_list = os.listdir(self.mask_path[0])
+          ex_paths = [os.path.join(self.mask_path[0], file) for file in ex_file_list]
+          ex_paths.sort()
+          
+          he_file_list = os.listdir(self.mask_path[1])
+          he_paths = [os.path.join(self.mask_path[1], file) for file in he_file_list]
+          he_paths.sort()
+          
+          me_file_list = os.listdir(self.mask_path[2])
+          me_paths = [os.path.join(self.mask_path[2], file) for file in me_file_list]
+          me_paths.sort()
+          
+          se_file_list = os.listdir(self.mask_path[3])
+          se_paths = [os.path.join(self.mask_path[3], file) for file in se_file_list]
+          se_paths.sort()
+          
+          mask_images_paths = [ _ for _ in zip(ex_paths, he_paths, me_paths, se_paths)]
+          
           # image와 label 길이가 같은지 확인
           assert len(input_images_paths) == len(mask_images_paths)
         
@@ -85,6 +106,7 @@ class DR_Generator(tf.keras.utils.Sequence):
           data_paths = [ _ for _ in zip(input_images_paths, mask_images_paths)]
         
         else:
+          # unsupervsion 떄는 필요없음
           data_paths = [_ for _ in zip(input_images_paths)]
         
         start, end = self.start_end_index
@@ -95,9 +117,9 @@ class DR_Generator(tf.keras.utils.Sequence):
         return math.ceil(len(self.data_paths) / self.batch_size)
 
     def __getitem__(self, index):
-        # 입력과 출력을 만드는 함수
-        # 입력은 전처리가 input image이고 
-        # 출력은 semantic label
+        '''
+        입력과 출력을 만드는 함수
+        '''
         batch_data_paths = self.data_paths[
                                index*self.batch_size:
                                (index + 1)*self.batch_size
@@ -105,36 +127,57 @@ class DR_Generator(tf.keras.utils.Sequence):
         
         # 채널 수 바꿔줘야 됨
         inputs = np.zeros([self.batch_size, *self.img_size])
-        outputs = np.zeros([self.batch_size, *self.img_size])
+        
+        if self.dataset == "FGADR":
+          # mask가 4개임 , HardExudate, Hemohedge, Microane, SoftExudates
+          ex = np.zeros([self.batch_size, *self.img_size])
+          he = np.zeros([self.batch_size, *self.img_size])
+          ma = np.zeros([self.batch_size, *self.img_size])
+          se = np.zeros([self.batch_size, *self.img_size])
         
         for i, data in enumerate(batch_data_paths):
             # supervsion 일때는 4개의 label
             # unsupervision 일때는 label이 image가 됨 
-            if self.mask_path:
-              input_img_path, output_path = data
+            if self.use_mask:
+              input_img_path, output_paths = data
+              
+              # mask : HardExudate, Hemohedge, Microane, SoftExudates
+              _ex = preprocess_image(output_paths[0], img_size=self.img_size, use_hist=False)
+              _ex = _ex / 255.0
+              ex[i] = _ex
+              
+              _he = preprocess_image(output_paths[1], img_size=self.img_size, use_hist=False)
+              _he = _he / 255.0
+              he[i] = _he
+              
+              _ma = preprocess_image(output_paths[2], img_size=self.img_size, use_hist=False)
+              _ma = _ma / 255.0
+              ma[i] = _ma
+              
+              _se = preprocess_image(output_paths[3], img_size=self.img_size, use_hist=False)
+              _se = _se / 255.0
+              se[i] = _se
             
             else:
+              # mask 없음
               input_img_path,  = data
-              output = 0
+              
               
             # image
             _input = preprocess_image(input_img_path, img_size=self.img_size)
             # scale 0~255로 바꿔주기
             _input = _input / 255.0
-            
-            # mask
-            _output = 0
-            # _output = img_to_array(load_img(output_path, color_mode='grayscale'))
-            # _output = (_output==7).astype(np.uint8)*1
-            
+                        
             inputs[i] = _input
-            outputs[i] = _output
-        
+            
+        # mask 없을 때는 input이 label이 됨
         if self.dataset == "EyePacks":
            return inputs, inputs
         
-        return inputs, outputs
-
+        # input, [mask 4개]
+        if self.dataset == "FGADR":
+          return inputs, [ex, he, ma, se]
+        
     def on_epoch_end(self):
         # 한 epoch가 끝나면 실행되는 함수
         # 학습중인 경우에 순서를 random shuffle하도록 적용
