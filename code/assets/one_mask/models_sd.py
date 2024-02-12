29a54c403062b4fae5d96af47b21a1b6c9d5ea7c
mkk4726 <mkk4726@naver.com>
2024-02-12 17:35:13 +0000

    edit SD_unet

--- code/assets/one_mask/models_sd.py
+++ code/assets/one_mask/models_sd.py
@@ -1,6 +1,80 @@
-import tensorflow as tf
 from tensorflow import keras
-import keras_cv
+from keras import backend as K
+from scipy.stats import bernoulli
+
+# from keras.engine.topology import Layer
+# import keras_cv
+
+import tensorflow as tf
+import copy
+
+class DropBlock(keras.layers.Layer):
+    """
+    Regularization Technique for Convolutional Layers.
+
+    Pseudocode:
+    1: Input:output activations of a layer (A), block_size, γ, mode
+    2: if mode == Inference then
+    3: return A
+    4: end if
+    5: Randomly sample mask M: Mi,j ∼ Bernoulli(γ)
+    6: For each zero position Mi,j , create a spatial square mask with the center being Mi,j , the width,
+        height being block_size and set all the values of M in the square to be zero (see Figure 2).
+    7: Apply the mask: A = A × M
+    8: Normalize the features: A = A × count(M)/count_ones(M)
+
+    # Arguments
+        block_size: A Python integer. The size of the block to be dropped.
+        gamma: float between 0 and 1. controls how many activation units to drop.
+    # References
+        - [DropBlock: A regularization method for convolutional networks](
+           https://arxiv.org/pdf/1810.12890v1.pdf)
+    """
+    def __init__(self, block_size, keep_prob):
+        super(DropBlock, self).__init__()
+        self.block_size = block_size
+        self.keep_prob = keep_prob
+
+    def call(self, x, training=False):
+
+        # During inference, we do not Drop Blocks. (Similar to DropOut)
+        if not training:
+            return x
+
+        # Calculate Gamma
+        feat_size = int(x.shape[-1])
+        gamma = ((1-self.keep_prob)/(self.block_size**2)) * ((feat_size**2) / ((feat_size-self.block_size+1)**2))
+
+        padding = self.block_size//2
+
+        # Randomly sample mask
+        sample = bernoulli.rvs(size=(feat_size-(padding*2), feat_size-(padding*2)),p=gamma)
+
+        # The above code creates a matrix of zeros and samples ones from the distribution
+        # We would like to flip all of these values
+        sample = 1-sample
+
+        # Pad the mask with ones
+        sample = np.pad(sample, pad_width=padding, mode='constant', constant_values=1)
+
+        # For each 0, create spatial square mask of shape (block_size x block_size)
+        mask = copy.copy(sample)
+        for i in range(feat_size):
+            for j in range(feat_size):
+                if sample[i, j]==0:
+                    mask[i-padding : i+padding+1, j-padding : j+padding+1] = 0
+
+        mask = mask.reshape((1, feat_size, feat_size))
+
+        # Apply the mask
+        x = x * np.repeat(mask, x.shape[1], 0)
+
+        # Normalize the features
+        count = np.prod(mask.shape)
+        count_ones = np.count_nonzero(mask == 1)
+        x = x * count / count_ones
+
+        return x
 
 
 class ConvBlock(tf.keras.layers.Layer):
@@ -12,17 +86,19 @@ class ConvBlock(tf.keras.layers.Layer):
         self.relu = keras.layers.Activation('relu')
         if dropout_args != None:
             rate, block_size = dropout_args
-            self.dropout = keras_cv.layers.DropBlock2D(rate=rate, block_size=block_size)
+            keep_probs = 1 - rate
+            
+            self.dropout = DropBlock(keep_prob=keep_prob, block_size=block_size)         
             self.use_drop = True
         else:
             self.use_drop = False
         # self.dropout = keras_cv.layers.DropoutBlock2D(rate=0.08, block_size=7)
             
-    def call(self, x):
+    def call(self, x, training):
         x = self.conv(x)
         x = self.bn(x)
         if self.use_drop:
-            x = self.dropout(x)
+            x = self.dropout(x, training)
         x = self.relu(x)
         
         return x
@@ -37,11 +113,11 @@ class UpsampleBlock(tf.keras.layers.Layer):
         self.concat = keras.layers.Concatenate()
         self.conv_block_2 = ConvBlock(n_filters, dropout_args)
 
-    def call(self, x, skip):
+    def call(self, x, skip, training):
         x = self.conv_T(x)
-        x = self.conv_block_1(x)
+        x = self.conv_block_1(x, training)
         x = self.concat([x, skip])
-        x = self.conv_block_2(x)
+        x = self.conv_block_2(x, training)
 
         return x   
 
@@ -68,31 +144,31 @@ class EncoderBlock(tf.keras.layers.Layer):
         self.max_poolings_3 = keras.layers.MaxPooling2D(2)
         self.max_poolings_4 = keras.layers.MaxPooling2D(2)
         
-    def call(self, x):
+    def call(self, x, training):
         skips = []
         
-        x = self.conv_blocks_0_1(x)
-        x = self.conv_blocks_0_2(x)
+        x = self.conv_blocks_0_1(x, training)
+        x = self.conv_blocks_0_2(x, training)
         skips.append(x)
         x = self.max_poolings_1(x)
         
-        x = self.conv_blocks_1_1(x)
-        x = self.conv_blocks_1_2(x)
+        x = self.conv_blocks_1_1(x, training)
+        x = self.conv_blocks_1_2(x, training)
         skips.append(x)
         x = self.max_poolings_2(x)
         
-        x = self.conv_blocks_2_1(x)
-        x = self.conv_blocks_2_2(x)
+        x = self.conv_blocks_2_1(x, training)
+        x = self.conv_blocks_2_2(x, training)
         skips.append(x)
         x = self.max_poolings_3(x)
         
-        x = self.conv_blocks_3_1(x)
-        x = self.conv_blocks_3_2(x)
+        x = self.conv_blocks_3_1(x, training)
+        x = self.conv_blocks_3_2(x, training)
         skips.append(x)
         x = self.max_poolings_4(x)
         
-        x = self.conv_blocks_4_1(x)
-        x = self.conv_blocks_4_2(x)
+        x = self.conv_blocks_4_1(x, training)
+        x = self.conv_blocks_4_2(x, training)
 
         return x, skips
 
@@ -120,13 +196,13 @@ class DecoderBlock(tf.keras.layers.Layer):
                                                   activation='sigmoid')
         
 
-    def call(self, x, skips):
-        x = self.upsample_blocks_0(x, skips[0])
-        x = self.upsample_blocks_1(x, skips[1])
-        x = self.upsample_blocks_2(x, skips[2])
-        x = self.upsample_blocks_3(x, skips[3])
+    def call(self, x, skips, training):
+        x = self.upsample_blocks_0(x, skips[0], training)
+        x = self.upsample_blocks_1(x, skips[1], training)
+        x = self.upsample_blocks_2(x, skips[2], training)
+        x = self.upsample_blocks_3(x, skips[3], training)
         
-        x = self.last_conv(x)
+        x = self.last_conv(x, training)
         x = self.last_block(x)
         
         return x
@@ -149,15 +225,15 @@ class SMD_Unet(tf.keras.Model):
         self.decoder= DecoderBlock(self.dec_filters, dropout_args)
 
 
-    def call(self, inputs, only_recons=False):
+    def call(self, inputs, only_recons=False, training=False):
         # Encoder
-        x, skips = self.encoder(inputs)
+        x, skips = self.encoder(inputs, training)
         # Decoder
-        input_hat = self.reconstruction(x, skips[::-1])
+        input_hat = self.reconstruction(x, skips[::-1], training)
         # reconstruction만 학습할 때 구분
         if only_recons:     
             return [input_hat]
         else:
-            mask_hat = self.decoder(x, skips[::-1])
+            mask_hat = self.decoder(x, skips[::-1], training)
             
             return [input_hat, mask_hat]
